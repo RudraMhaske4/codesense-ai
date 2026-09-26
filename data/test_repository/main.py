3afedbea53a7a8d3f9c54207e0d004f808a50e4a
Rudra Mhaske <rudramhaske9999@gmail.com>
2026-09-26 13:04:47 +0530

    Add and test code quality risk detection

--- data/test_repository/main.py
+++ data/test_repository/main.py
@@ -4,3 +4,11 @@ def calculate_total(price, quantity):
     if quantity > 0:
         return price * quantity
     return 0
+def risky_function(a, b, c, d, e, f):
+    if a > 0:
+        if b > 0:
+            if c > 0:
+                if d > 0:
+                    if e > 0:
+                        return f
+    return 0
